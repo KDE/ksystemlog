04a39d0aeb8e3466ee38ecc726a36c8003b01e24
Laurent Montel <montel@kde.org>
2008-07-08 07:44:01 +0000

    Fix extract messages
    svn path=/trunk/KDE/kdeadmin/ksystemlog/; revision=829339

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-$EXTRACTRC */*.kcfg >> rc.cpp || exit 11
-$EXTRACTRC src/*.rc >> rc.cpp || exit 12
-$EXTRACTRC src/*.ui >> rc.cpp || exit 13
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp"` -o $podir/ksystemlog.pot
 rm -f rc.cpp
