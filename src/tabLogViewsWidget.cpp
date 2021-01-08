9442ce88495ac03e6f54b6fbfd4e44cc2f59261a
Laurent Montel <montel@kde.org>
2021-01-08 13:24:36 +0100

    Debug--

--- src/tabLogViewsWidget.cpp
+++ src/tabLogViewsWidget.cpp
@@ -75,7 +75,6 @@ TabLogViewsWidget::TabLogViewsWidget(QWidget *parent)
 
 TabLogViewsWidget::~TabLogViewsWidget()
 {
-    qDebug() << " TabLogViewsWidget::~TabLogViewsWidget()" << this;
     const QList<TabLogManager *> copy = mTabLogManagers;
 
     for (TabLogManager *tabLogManager : copy) {
