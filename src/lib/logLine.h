dd343f467034878986448a87e977d64e2951e0f5
Laurent Montel <montel@kde.org>
2021-04-08 13:47:10 +0200

    Don't forward declaration when we include file

--- src/lib/logLine.h
+++ src/lib/logLine.h
@@ -29,8 +29,6 @@
 #include "logLevel.h"
 
 class LogViewWidget;
-class QDateTime;
-class QStringList;
 class LogViewWidgetItem;
 
 class LogLine
--- src/lib/processOutputLogFileReader.h
+++ src/lib/processOutputLogFileReader.h
@@ -29,7 +29,6 @@
 
 #include "logFileReader.h"
 
-class QProcess;
 
 class ProcessOutputLogFileReaderPrivate;
 
