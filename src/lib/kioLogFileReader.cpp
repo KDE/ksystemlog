ae84fb5975b2336c744511644faec88eaf8706d2
Laurent Montel <montel@kde.org>
2021-06-21 13:50:15 +0200

    We depend against kf5.83/qt5.15

--- src/lib/kioLogFileReader.cpp
+++ src/lib/kioLogFileReader.cpp
@@ -60,11 +60,7 @@ void KioLogFileReader::open()
     connect(mFileJob, SIGNAL(close(KIO::Job *)), this, SLOT(closeDone(KIO::Job *)));
 
     connect(mFileJob, &KIO::FileJob::data, this, &KioLogFileReader::dataReceived);
-#if KIO_VERSION < QT_VERSION_CHECK(5, 78, 0)
-    connect(mFileJob, &KIO::FileJob::mimetype, this, &KioLogFileReader::mimetypeReceived);
-#else
     connect(mFileJob, &KIO::FileJob::mimeTypeFound, this, &KioLogFileReader::mimetypeReceived);
-#endif
     logDebug() << "File opened.";
 }
 
